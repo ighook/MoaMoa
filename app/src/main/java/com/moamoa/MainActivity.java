d73a88475f6dd832599b5eab24f08cb724efc14d
ighook <kaliv_@naver.com>
2020-10-06 15:51:55 +0900

    2020.10.06 15:51

--- app/src/main/java/com/moamoa/MainActivity.java
+++ app/src/main/java/com/moamoa/MainActivity.java
@@ -1,15 +1,30 @@
 package com.moamoa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void ClickMenu(View view) {
+        openDrawer(drawerLayout);
+    }
+
+    private static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
     }
 }
