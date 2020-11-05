d5534eeb018351692a3e36ac38436f5ac2e6ee6c
ighook <kaliv_@naver.com>
2020-11-06 06:26:29 +0900

    2020.10.07 13:42

--- app/src/main/java/com/moamoa/LoginActivity.java
+++ app/src/main/java/com/moamoa/LoginActivity.java
@@ -20,7 +20,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText input_id, input_pw;
+    private EditText et_id, et_pw;
     public Button btn_login, btn_register;
 
     @Override
@@ -28,26 +28,29 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        input_id = findViewById(R.id.id);
-        input_pw = findViewById(R.id.password);
+        et_id = findViewById(R.id.et_id);
+        et_pw = findViewById(R.id.et_pw);
         btn_login = findViewById(R.id.btn_login);
         btn_register = findViewById(R.id.btn_register);
 
+        // 회원가입 버튼
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterPersonalActivity.class);
-                startActivity(intent);
-                finish();
+            Intent intent = new Intent(LoginActivity.this, RegisterPersonalActivity.class);
+            startActivity(intent);
+            finish();
             }
         });
 
+        // 로그인 버튼
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String userID = input_id.getText().toString();
-                String userPW = input_pw.getText().toString();
+                String userID = et_id.getText().toString();
+                String userPW = et_pw.getText().toString();
 
+                // 로그인을 눌렀을 때 아이디와 비밀번호 입력칸이 비어있는지 확인
                 if(userID.isEmpty() || userPW.isEmpty()) {
                     Toast.makeText(getApplicationContext(), "아이디와 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show();
                     return;
